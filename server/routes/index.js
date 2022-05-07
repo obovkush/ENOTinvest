3c983b6e90f09758715b71b158b4bb9726740357
Oleg Bovkush <obovkush@gmail.com>
2022-05-08 01:59:07 +1000

    Сервер - рег-ия, автор-ия, активация по емэйл

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,10 @@
-const express = require('express');
-const { body } = require('express-validator');
-const userController = require('../controllers/userController');
-const authMiddleware = require('../middlewares/authMiddleware');
+const router = require('express').Router();
 
-const router = express.Router();
+const userRouter = require('./userRouter');
 
-router.post(
-  '/registration',
-  body('name').isLength({ min: 1, max: 32 }),
-  body('email').isEmail(),
-  body('password').isLength({ min: 3, max: 32 }),
-  userController.registration,
-);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.get('/activate/:link', userController.activate);
-router.get('/refresh', userController.refresh);
-router.get('/users', authMiddleware, userController.getUsers);
+router.use('/user', userRouter);
+router.get('/', (req, res) => {
+  res.render('index');
+});
 
 module.exports = router;
