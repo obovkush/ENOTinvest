af6822bdd560a631a8c4ac54c5d4bb18225d3570
Maks <dyupriz89evms@mail.ru>
2022-05-21 00:04:18 +0300

    double minus edits in stocks and refactoring

--- server/client/src/components/StockAccordion/StockAccordion.jsx
+++ server/client/src/components/StockAccordion/StockAccordion.jsx
@@ -2,18 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import Diagram from '../Diagram/Diagram'
-
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Typography,
-  LinearProgress,
-  Box,
-  Grid,
-  Paper,
-  TableRow,
-} from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Typography, LinearProgress, Box, Grid, Paper, TableRow } from '@mui/material';
 import AddTaskOutlinedIcon from '@mui/icons-material/AddTaskOutlined';
 import StraightOutlinedIcon from '@mui/icons-material/StraightOutlined';
 import { Badge } from 'antd';
