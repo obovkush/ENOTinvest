802f4022f61d5270bf169703553522ed471c6948
Oleg Bovkush <obovkush@gmail.com>
2022-05-22 22:55:15 +1000

    Fix refresh-token

--- server/server.js
+++ server/server.js
@@ -24,8 +24,12 @@ app.use((req, res, next) => {
   const origin = req.get('origin');
   if (accessList.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Headers', 'Content-type');
     res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
   }
   next();
 });
