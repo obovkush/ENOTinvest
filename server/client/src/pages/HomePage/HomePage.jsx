2a77afb2187a1e628e02d67af5e721581f81360b
Falcon <baz_raider@mail.ru>
2022-05-26 00:24:13 +0700

    add orange card on main page with briefcase status

--- server/client/src/pages/HomePage/HomePage.jsx
+++ server/client/src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Grid } from '@mui/material';
 import SliderForNewsAndYouTube from '../../components/SliderForNewsAndYouTube/SliderForNewsAndYouTube';
-import StockAccordion from '../../components/StockAccordion/StockAccordion';
 import Best from '../../components/StocksPacks/Best';
 import Worst from '../../components/StocksPacks/Worst';
-import axios from 'axios';
-
+import MainPageHeader from '../../components/MainPageHeader/MainPageHeader';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
 
 function HomePage() {
+
+  const informationForCards = [
+    {
+      name: 'firstCard',
+      displayName: 'Главная',
+      color: '#ca441a',
+      info: 1000,
+      additionalInfo: 'Состояние портфеля',
+      icon: <BusinessCenterIcon sx={{ fill: '#ffffff', fontSize: 30 }} />,
+    },
+    {
+      name: 'secondCard',
+      displayName: 'Акции',
+      color: '#f07800',
+      info: 2000,
+      additionalInfo: 'Состав кошелька',
+      icon: <AccountBalanceWalletIcon sx={{ fill: '#ffffff', fontSize: 30 }} />,
+    },
+    {
+      name: 'thirdCard',
+      displayName: 'Портфель',
+      color: '#ffa801',
+      info: 3000,
+      additionalInfo: 'Результаты акций',
+      icon: <ShowChartIcon sx={{ fill: '#ffffff', fontSize: 30 }} />,
+    },
+  ]
+
+  const cardLineStyles = {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+  }
 
   return (
     <>
       <Grid container spacing={2} pt={2} >
         <Grid item xs={12} sm={12} md={12} lg={7} xl={8}>
-          <h1>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={cardLineStyles}>
+            {informationForCards.map(elem => {
+              return (
+                <MainPageHeader elem={elem} />
+              )
+            })
+            }
+          </Grid>
+          <h1 style={{ marginTop: '1rem' }}>
             Лучшие акции за сегодня
           </h1>
           <Best />
-          <h1>
+          <h1 style={{ marginTop: '1rem' }}>
             Лучшие падения за сегодня
           </h1>
           <Worst />
-  
         </Grid>
         <Grid item xs={12} sm={12} md={7} lg={5} xl={4}>
           <SliderForNewsAndYouTube />
