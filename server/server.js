2dae8f65fd510888e108d11eb54ee08e244badd3
Oleg Bovkush <obovkush@gmail.com>
2022-05-26 17:30:18 +1000

    Сервер: CORS polisy DELETE

--- server/server.js
+++ server/server.js
@@ -23,7 +23,10 @@ app.use((req, res, next) => {
   if (accessList.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, HEAD, OPTIONS, POST, DELETE',
+    );
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
