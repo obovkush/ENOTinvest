9edec163c7fc78d9568368e1e0f3cb2baa85df07
Oleg Bovkush <obovkush@gmail.com>
2022-05-26 16:40:19 +1000

    Клиент: по клику добавляет в избранное

--- server/client/src/components/FavoriteButton/FavoriteButton.jsx
+++ server/client/src/components/FavoriteButton/FavoriteButton.jsx
@@ -1,17 +1,26 @@
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
 import { IconButton } from '@mui/material';
-import { DeleteIcon } from '@mui/icons-material';
 import BookmarkAddOutlinedIcon from '@mui/icons-material/BookmarkAddOutlined';
 
 function FavoriteButton() {
-  const handleClick = (event) => {
+  const user = useSelector((state) => state.user);
+  const stocks = useSelector((state) => state.stocks);
+
+  const handleCreateFavorite = (event) => {
     event.stopPropagation();
+    console.log('work');
+    axios.post(
+      `${process.env.REACT_APP_API_URL}api/favorite/user/${user.id}/stock/${stocks[5].secid}/create`,
+    );
   };
 
   return (
     <>
       <IconButton
         aria-label="delete"
-        onClick={handleClick}
+        onClick={handleCreateFavorite}
         sx={{ display: 'inline-block' }}
       >
         <BookmarkAddOutlinedIcon />
